68ae23032f0e9c51a496bb2d942cb43ddb1a2168
smister <398009951@qq.com>
2016-06-24 12:01:42 +0800

    first test

---
+++ stest/test/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/24/16
+ * Time: 10:26 AM
+ */
+
+namespace stest;
+
+
+class Test
+{
+    public function pt()
+    {
+        echo 'hello word';
+    }
+}
